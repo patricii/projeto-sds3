b394f087aa6d669301ec8cfadcc8cad51aa527eb
thqv74 <thqv74@gmail.com>
2021-05-03 21:03:08 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/patricii" target="_blank" rel="noreferrer">Adriano Patricio</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Linkedin para contato: <a href="https://www.linkedin.com/in/eng-adriano-patricio/" target="_blank" rel="noreferrer">@eng-adriano-patricio</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
