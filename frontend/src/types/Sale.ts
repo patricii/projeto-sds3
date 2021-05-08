aa935c483551c369b1af113bcd54466e03873d69
thqv74 <thqv74@gmail.com>
2021-05-07 21:16:05 -0300

    Bar Chart integration

--- frontend/src/types/Sale.ts
+++ frontend/src/types/Sale.ts
@@ -1,5 +1,13 @@
 export type SaleSum = {
-    sallerName: string,
-    sum: number
+    sallerName: string;
+    sum: number;
+
+}
+
+export type SaleSuccess = {
+
+    sallerName: string;
+    visited: number;
+    deals: number;
 
 }
