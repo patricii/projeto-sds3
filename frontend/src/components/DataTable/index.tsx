ba4b04bfb1e880653b71a2a6328288eff42770df
thqv74 <thqv74@gmail.com>
2021-05-08 10:27:49 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,10 +1,13 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "Utils/format";
 import { BASE_URL } from "Utils/requests";
 
 const DataTable = () => {
+
+    const [activePage, setActivePage] = useState(0);
 
     const [page, setPage] = useState<SalePage>({
         first: true,
@@ -13,15 +16,22 @@ const DataTable = () => {
         totalElements: 0,
         totalPages: 0
     })
-
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+        axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
             .then(response => {
                 setPage(response.data);
             });
 
-    }, []);
+    }, [activePage]);
+
+    const changePage = (index: number) =>{
+
+        setActivePage(index);
+    }
+
     return (
+        <>
+        <Pagination page={page} onPageChange={changePage}/>
         <div className="table-responsive">
             <table className="table table-striped table-sm">
                 <thead>
@@ -34,7 +44,7 @@ const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {page.content?.map(item => {
+                    {page.content?.map(item => (
                         <tr key={item.id}>
                             <td>{formatLocalDate(item.date, 'dd/MM/yyyy')}</td>
                             <td>{item.seller.name}</td>
@@ -42,10 +52,11 @@ const DataTable = () => {
                             <td>{item.deals}</td>
                             <td>{item.amount.toFixed(2)}</td>
                         </tr>
-                    })}
+                    ))}
                 </tbody>
             </table>
         </div>
+        </>
 
     );
 }
